26d192f39bb4d85149678651e83edb5018748134
Your Name <your_email@example.com>
2025-05-07 18:25:46 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo"Hello World<br>";
+echo"Liverpool are champions";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order Pizza</button>
+</body>
+</html>

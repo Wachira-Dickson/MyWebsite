7ae0a82968194ef9ea0774ec886d1d2c6fec11ec
Your Name <your_email@example.com>
2025-05-07 19:51:07 +0300

    Introduction to variables in php

--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <?php
-echo"Hello World<br>";
-echo"Liverpool are champions";
+    $name = "Dickson Wachira";
+    $food = "Pizza";
+    $email = "wachira789@gmail.com";
+
+    $age  = 21;
+    $users = 2;
+    $quantity = 5;
+
+    $gpa = 2.5;
+    $price = 4.99;
+    $tax_rate = 5.1;
+
+    $employed = true;
+    $online = false;
+    $for_sale = true;
+
+    $total = null;
+
+    echo "Hello {$name}<br>";
+    echo "You like {$food}<br>";
+    echo "Your email is {$email}<br>";
+
+    echo "You are {$age} years old<br>";
+    echo "There are {$users} users online<br>";
+    echo "You would like to buy {$quantity} items<br>";
+
+    echo"Your gpa is:{$gpa}<br>";
+    echo"Your pizza costs \${$price}<br>";
+    echo"The sales tax rate is : {$tax_rate}%<br>";
+
+    echo"Online status: {$online}<br>";
+    echo"Employment status: {$employed}<br>";
+
+    echo"You have ordered {$quantity} * {$food}<br>";
+    $total = $quantity * $price;
+    echo"Your total is: \${$total}<br>";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <br>
-    <button>Order Pizza</button>
-</body>
-</html>

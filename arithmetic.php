45abfd857bdcb3c97f6f0134d8404091daf46808
Your Name <your_email@example.com>
2025-05-07 20:08:26 +0300

    Basic arithmetic in php

---
+++ arithmetic.php
@@ -0,0 +1,26 @@
+<?php
+    //Arithmetic Operators
+    // - / * % ** +
+
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    //$z = $x + $y;
+    //$z = $x / $y;
+    //$z = $x * $y;
+    //$z = $x - $y;
+    //$z = $x ** $y;
+    $z = $x % $y;
+    echo $z;
+?>
+
+<?php
+    //Increment/Decrement Operators
+
+    $counter = 0;
+
+    $counter--;
+    echo $counter;
+
+?>

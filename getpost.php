236737b2fc54c050b7fecf8933e31d4f451fdec7
Your Name <your_email@example.com>
2025-05-07 21:02:45 +0300

    Create an order page for a restaurant

--- getpost.php
+++ getpost.php
@@ -7,15 +7,23 @@
 </head>
 <body>
     <form action="getpost.php" method="post">
-        <label>Username:</label><br>
-        <input type="text" name="username"><br>
-        <label>Password:</label><br>
-        <input type="password" name="password"><br>
-        <input type="submit" value="Log in">
+        <label>Quantity:</label><br>
+        <input type="text" name="quantity">
+        <input type="submit" value="total">
+
     </form>
 </body>
 </html>
 <?php
-    echo "{$_POST["username"]} <br>";
-    echo "{$_POST["password"]} <br>";
+    $item = "pizza";
+    $price = 5.99;
+    $quantity = $_POST["quantity"];
+    $total = null;
+
+    $total = $quantity * $price;
+
+    echo"You have ordered {$quantity} x {$item}s <br>";
+    echo"Your total is :\${$total}";
 ?>
+
+    

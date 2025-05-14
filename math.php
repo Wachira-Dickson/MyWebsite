4240ae4323eea0c606989bfdf1312a7b48c76a09
Your Name <your_email@example.com>
2025-05-14 20:05:37 +0300

    Calculate the circle using math functions

--- math.php
+++ math.php
@@ -6,33 +6,29 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="math.php" method="post">
-       <label>X:</label>
-       <input type="text" name="x">
-       <label>Y:</label>
-       <input type="text" name="y">
-       <label>Z:</label>
-       <input type="text" name="z">
-       <input type="submit" value="total">
-    </form>
+    <form action="index.php" method="post">
+        <label>radius:</label>
+        <input type="text" name="radius">
+        <input type="submit" value="calculate">
+    </form><br>
 </body>
 </html>
 <?php
-    $x = $_POST["x"];
-    $y = $_POST["y"];
-    $z = $_POST["z"];
-    $total = null;
+    $radius = $_POST["radius"];
+    $circumference = null;
+    $area = null;
+    $volume = null;
 
-    //$total = abs($x);
-    //$total = round($x);
-    //$total = floor($x);
-    //$total = ceil($x);
-    //$total = pow($x, $y);
-    //$total = sqrt($x);
-    //$total = max($x, $y, $z);
-    //$total = pi();
-    //$total = min($x, $y, $z);
-    $total = rand(1, 6);
+    $circumference = 2 * pi() * $radius;
+    $circumference = round($circumference, 2);
 
-    echo $total
+    $area = pi() * pow($radius, 2);
+    $area = round($area, 2);
+
+    $volume = 4/3 * pi() * pow($radius, 3);
+    $volume = round($volume, 2);
+
+    echo"Circumference = {$circumference}cm <br>";
+    echo"Area = {$area}cm <br>";
+    echo"Volume = {$volume}cm <br>";
 ?>
